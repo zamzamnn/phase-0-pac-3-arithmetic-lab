82cf481078a184520bff138b2663b86f5f362b98
zamzamnn <zamzamnoorahmed73@gmail.com>
2024-05-09 08:35:32 +0300

    complete lab arithmatic

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a,b) {
+    // we'll fill this in shortly
+  }
+  function subtract(a,b) {
+    return a-b;
+    
+    // we'll fill this in shortly
+  }
+  function multiply(a,b) {
+    return a*b;
+    // we'll fill this in shortly
+  }
+  function divide(a,b) {
+    // we'll fill this in shortly
+    return a/b;
+  }
+  function add(a, b) {
+    return a + b;
+  }
+function  increment(n){
+  return n+1;
+}
+function  decrement(n){
+  return n-1;
+}
+function makeInt(string){
+  return parseInt(string ,10);
+}
+function preserveDecimal(string){
+  return parseFloat(string);
+}
+
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
